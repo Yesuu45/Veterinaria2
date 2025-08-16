a81dfa7ab3c03590f94ad13288e12303d0019777
Yesuu45 <yesuue.arost@uqvirtual.edu.co>
2025-08-16 00:03:48 -0500

    6.0

---
+++ src/test/java/co/edu/uniquindio/poo/veterinaria/model/VeterinariaTest.java
@@ -0,0 +1,55 @@
+package co.edu.uniquindio.poo.veterinaria.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class VeterinariaTest {
+
+    @Test
+    void agregarCliente() {
+    }
+
+    @Test
+    void agregarVeterinario() {
+    }
+
+    @Test
+    void agregarPersonalApoyo() {
+    }
+
+    @Test
+    void verificarVeterinario() {
+    }
+
+    @Test
+    void verificarPersonalApoyo() {
+    }
+
+    @Test
+    void verificarPropietarios() {
+    }
+
+    @Test
+    void eliminarPropietario() {
+    }
+
+    @Test
+    void eliminarVeterinario() {
+    }
+
+    @Test
+    void eliminarPersonalApoyo() {
+    }
+
+    @Test
+    void actualiazarPersonalApoyo() {
+    }
+
+    @Test
+    void actualiazarVeterinario() {
+    }
+
+    @Test
+    void actualiazarPropietario() {
+    }
+}
